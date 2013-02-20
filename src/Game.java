019c4bf6d1a22e905bfc3d1acd0432a272e383e3
bartwe <bluelive@gmail.com>
2013-02-20 21:19:49 +0100

    Multicolored triangle of DOOOM!

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
 
 import java.awt.*;
 
@@ -10,6 +11,7 @@ public class Game {
     private int width;
     private int height;
     private Thread sleeperThread;
+    private boolean requestResetViewPort;
 
     public void startLWJGL(final Canvas display_parent) {
         this.display_parent = display_parent;
@@ -20,10 +22,7 @@ public class Game {
                     Display.setParent(display_parent);
                     Display.setTitle("ardust");
                     Display.create();
-                    width = display_parent.getWidth();
-                    height = display_parent.getHeight();
-                    GL11.glViewport(0, 0, width, height);
-//                    initGL();
+                    resetViewPort();
                 } catch (Throwable e) {
                     e.printStackTrace();
                     return;
@@ -52,7 +51,7 @@ public class Game {
         try {
             if (gameThread != null)
                 gameThread.join();
-            if (sleeperThread != null)      {
+            if (sleeperThread != null) {
                 sleeperThread.interrupt();
                 sleeperThread.join();
             }
@@ -74,10 +73,94 @@ public class Game {
     }
 
     public void resized() {
+        requestResetViewPort = true;
+    }
 
+    void resetViewPort() {
+        width = display_parent.getWidth();
+        height = display_parent.getHeight();
+        GL11.glViewport(0, 0, width, height);
+        setupRenderMode();
+    }
+
+    private void setupRenderMode() {
+        Display.setVSyncEnabled(true);
+        GL11.glClearColor(0f, 0f, 0f, 0f);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        //GL11.glAlphaFunc(GL11.GL_GEQUAL, 0.8f); // cutoff ?
+
+        // for cursor
+        GL11.glLogicOp(GL11.GL_XOR);
+
+        GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+
+
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glHint(GL11.GL_PERSPECTIVE_CORRECTION_HINT, GL11.GL_NICEST);
+        GL11.glHint(GL11.GL_POLYGON_SMOOTH_HINT, GL11.GL_NICEST);
+
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+
+        // flat zero based surface, 0x0 at the top left
+        GL11.glColor4f(1f, 1f, 1f, 1f);
+        GL11.glMatrixMode(GL11.GL_PROJECTION);
+        GL11.glLoadIdentity();
+        GL11.glOrtho(0, width, height, 0, -1, 1);
+        GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        GL11.glLoadIdentity();
     }
 
     private void gameLoop() {
+        try {
+            while (running) {
+
+                if (requestResetViewPort) {
+                    requestResetViewPort = false;
+                    resetViewPort();
+                }
+
+                //update
+
+                //soundmanager.tick();
+
+                //clear
+                GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT + GL11.GL_COLOR_BUFFER_BIT); // assuming we need one
+
+                //render
+
+                GL11.glBegin(GL11.GL_TRIANGLES);
+
+                GL11.glColor4f(1f,0f,0f,1f);
+                GL11.glVertex2d(0,0);
+                GL11.glColor4f(0f,1f,0f,1f);
+                GL11.glVertex2d(width/2f,0);
+                GL11.glColor4f(0f,0f,1f,1f);
+                GL11.glVertex2d(0,height/2f);
+
+                GL11.glEnd();
+
+                //flip
+
+                Thread.yield();
+                Display.update(false);
+                if (Display.isCloseRequested()) {
+                    running = false;
+                }
+                // sync
+
+                Display.processMessages();
+                processKeyboardAndMouse();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
+    private void processKeyboardAndMouse() {
+
+    }
 }
