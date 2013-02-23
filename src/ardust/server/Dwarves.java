351d9ff74627cb1b5e836f715611dabf9ee6b792
bartwe <bluelive@gmail.com>
2013-02-23 20:23:49 +0100

    Only control your own dwarves.

--- src/ardust/server/Dwarves.java
+++ src/ardust/server/Dwarves.java
@@ -39,7 +39,7 @@ public class Dwarves {
 
                 break;
             case CraftArmor:
-                entity.
+//                entity.
                 break;
             case CraftSword:
 
